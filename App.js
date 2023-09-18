eea8f5db125ce2ad76c71315cb8f4a3e7ad1cf47
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-09-18 16:53:17 +0200

    hw2 start

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: "column",
+        },
+      ]}
+    >
+      <View style={{ flex: 2, backgroundColor: "red" }} />
+      <View style={{ flex: 1, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 5, backgroundColor: "green" }} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
+
+export default Flex;
